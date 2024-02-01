6ec50799a38fd2e8f1f85845e87763364eb981bc
mirek33g <mirek33g@gmial.com>
2024-02-01 09:35:01 +0000

    Changed to use SQLAlchemy database

--- main.py
+++ main.py
@@ -3,14 +3,38 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
+from flask_sqlalchemy import SQLAlchemy
 import csv
 
+#https://www.google.com/maps/place/Manchester
 
 # Flask  Application Setup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 bootstrap = Bootstrap5(app)
 
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///caffees.db"
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class Caffees(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cafe = db.Column(db.String(250), nullable=False, unique=True)
+    location = db.Column(db.String(250), nullable=False)
+    open = db.Column(db.String, nullable=False)
+    close = db.Column(db.String, nullable=False)
+    coffee_rating = db.Column(db.String, nullable=False)
+    wifi_rating = db.Column(db.String, nullable=False)
+    power = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Caffee {self.cafe}>'
+
+
+with app.app_context():
+    db.create_all()
 
 # creates a form for user to add a new cafe
 class CafeForm(FlaskForm):
@@ -54,66 +78,57 @@ def add_cafe():
     # Checks if form is submitted
     if form.validate_on_submit():
         # Gets data from the form
-        with open('cafe-data.csv', 'a') as csv_file:
-            # Writes data to the csv
-            csv_writer = csv.writer(csv_file, delimiter=',')
-            # Writes data to the csv
-            csv_writer.writerow([
-                form.cafe.data,
-                form.location.data,
-                form.open.data,
-                form.close.data,
-                form.coffee_rating.data,
-                form.wifi_rating.data,
-                form.power.data])
+        with app.app_context():
+            new_cafe = Caffees(
+                cafe=form.cafe.data,
+                location=form.location.data,
+                open=form.open.data,
+                close=form.close.data,
+                coffee_rating=form.coffee_rating.data,
+                wifi_rating=form.wifi_rating.data,
+                power=form.power.data)
+            db.session.add(new_cafe)
+            db.session.commit()
+
             # Redirects to the home page
         return redirect(url_for('cafes'))
+
     else:
         return render_template('add.html', form=form)
-
 
 
 # Deletes cafe from the file
 @app.route('/delete', methods=['GET', 'POST'])
 def delete():
+
     # Creates form
     del_form = DelForm()
     # Checks if form is submitted
     if del_form.validate_on_submit():
         # Gets data from the form
-        cafe_name = del_form.cafe.data.strip().title()
-        rows = []
-        # Deletes data from the csv
-        with open('cafe-data.csv', 'r', newline='') as file:
-            reader = csv.reader(file)
-            # Writes data to the csv
-            for row in reader:
-                if row[0] != cafe_name:
-                    rows.append(row)
-        # Writes data to the csv
-        with open('cafe-data.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(rows)
-        # Redirects to the home page
+        cafe_name = del_form.cafe.data
+        with app.app_context():
+            del_cafe = Caffees.query.filter_by(cafe=cafe_name).first()
+            db.session.delete(del_cafe)
+            db.session.commit()
         return redirect(url_for('cafes'))
 
+    # Redirects to the home page
     return render_template('delete.html', form=del_form)
 
 
 # Displays all the cafes
 @app.route('/cafes')
 def cafes():
-    # Gets data from the csv
-    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
-        list_of_rows = []
-        # Writes data to the csv
-        for row in csv_data:
-            list_of_rows.append(row)
-            # Redirects to the home page
-    return render_template('cafes.html', cafes=list_of_rows)
+    all_cafes = Caffees.query.all()
+    if not all_cafes:
+        return render_template('cafes.html', cafes=[])
+    else:
+        return render_template('cafes.html', cafes=all_cafes)
 
 
 # Runs Main Function
 if __name__ == '__main__':
     app.run(debug=True)
+
+
